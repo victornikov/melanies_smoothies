336511286946ecc2dc35c73ff0d45ab379050281
victornikov <69169252+victornikov@users.noreply.github.com>
2025-06-13 15:33:42 +0200

    Update streamlit_app.py
    Addd in For

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,14 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        smoothiefroot_response = requests.get ("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.write(ingredients_string)
 
 
-    smoothiefroot_response = requests.get ("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
-    st.text(smoothiefroot_response.json())
+
+#    st.text(smoothiefroot_response)
+#    st.text(smoothiefroot_response.json())
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
