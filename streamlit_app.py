c1efe47a57aa13a860532c3402e5d87fe79b7c4b
victornikov <69169252+victornikov@users.noreply.github.com>
2025-06-13 16:07:19 +0200

    Update streamlit_app.py
    commit 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col ('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop
+# VCV commit
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
